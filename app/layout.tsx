be0810cd167d2a81e737711695d5f9cc76d72d10
Joe Panetta <jlpanetta1681@gmail.com>
2024-12-19 20:00:05 -0500

    prismic and slice machine running, working on building homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-slate-900 text-slate-100" >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,10 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/slices"
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
     },
-  },
+  
   plugins: [],
 } satisfies Config;
